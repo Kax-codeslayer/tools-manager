4ca26190b0d8952feaacd7e7e05feea6f71cfa62
Christa848 <78590853+Christa848@users.noreply.github.com>
2021-03-02 16:49:18 +0200

    ticket changes

--- src/views/ticketsolve.vue
+++ src/views/ticketsolve.vue
@@ -3,20 +3,13 @@
 
 
 <div class="text-center">
-    <div>
+    <div id="navi">
   <b-navbar toggleable="sm"  class="nav2">
     <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
       
        
 <b-navbar-brand>
-    <b-form-checkbox
-      id="checkbox-2"
-      v-model="status"
-      name="checkbox-2"
-      value="accepted"
-      unchecked-value="not_accepted"
-    >
-       </b-form-checkbox>
+   <b-icon star></b-icon>
        </b-navbar-brand>
     <b-navbar-brand><b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Reply</b-button></b-navbar-brand>
     <b-navbar-brand><b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Add-Note</b-button></b-navbar-brand>
@@ -30,8 +23,9 @@
   </b-navbar>
 </div>
 
-    <b-row text="left">
-    <b-col cols="6">
+    <b-row id="ket">
+      <div id="ticket">
+    <b-col cols="">
         <b-row>
             <b-col cols="3" >
 <b-icon icon="envelope"></b-icon>
@@ -44,7 +38,7 @@
 
  <b-row>
             <b-col cols="3">
- <span class="badge badge-secondary">C</span>
+ <span class="badge badge-success">C</span>
             </b-col>
 
             <b-col cols="">
@@ -61,33 +55,25 @@
             <b-col>
                <p><i>Heey There</i></p>
  <p> My car has been oofline for days now and i havent been receiving my my reports. </p> 
+           <p><i>Regards</i></p>Kikie
                 </b-col>
         </b-row>
 
-           <b-row>
-            <b-col cols="3">
-<span class="badge badge-secondary">C</span>
-            </b-col>
-
-            <b-col>
-               
-<b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Reply</b-button>
-<b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Add Note</b-button>
-<b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Forward</b-button>
-                </b-col>
-        </b-row>
 
     </b-col>
-    <b-col cols="3" >
+    </div>
+    
+    <div id="res">
+    <b-col cols="" >
 <div>
-    <div>
+    
     <h5>Open</h5>
-    <p>FIRST RESPONSE DUE</p>
-    <P>by 20 April 2020,10:00</P></div>
+    FIRST RESPONSE DUE
+    <P> <small><b-icon icon="clock" animation="spin" font-scale="1" shift-v="6"></b-icon>  by 20 April 2020,10:00</small></P></div>
     <div>
     
     RESOLUTION  DUE  <i>Edit</i>
-   <p> <small>by 20 April 2020,10:00</small></p></div>
+   <p> <small> <b-icon icon="clock" animation="spin" font-scale="1" shift-v="6"></b-icon>  by 20 April 2020,10:00</small></p></div>
     <hr>
   <b-form >
     <label><small>Tags</small></label>
@@ -95,53 +81,56 @@
     
     
      <label for="input-with-list"><small>Type</small></label>
-    <b-form-input list="input-list" id="input-with-list"></b-form-input>
-    <b-form-datalist id="input-list" :options="options"></b-form-datalist>
+    <b-form-input list="input-list" id="input-with-list1"></b-form-input>
+    <b-form-datalist id="input-list" :options="['Question', 'Insident', 'Problem','Refunts']"></b-form-datalist>
 
      <label for="input-with-list"><small>Status</small></label>
-    <b-form-input list="input-list" id="input-with-list"></b-form-input>
-    <b-form-datalist id="input-list" :options="option"></b-form-datalist>
+    <b-form-input list="input-list" id="input-with-list2"></b-form-input>
+    <b-form-datalist id="input-list" :options="['Open', 'Pending', 'Resolved','Closed' ,'Waiting on Customer']" ></b-form-datalist>
 
      <label for="input-with-list"><small>Priority</small></label>
-    <b-form-input list="input-list" id="input-with-list"></b-form-input>
-    <b-form-datalist id="input-list" :options="option"></b-form-datalist>
+    <b-form-input list="input-list" id="input-with-list3"></b-form-input>
+    <b-form-datalist id="input-list" :options="['Low', 'Medium', 'High','Urgent' ]"></b-form-datalist>
 
      <label for="input-with-list"><small>Group</small></label>
-    <b-form-input list="input-list" id="input-with-list"></b-form-input>
-    <b-form-datalist id="input-list" :options="option"></b-form-datalist>
+    <b-form-input list="input-list" id="input-with-list4"></b-form-input>
+    <b-form-datalist id="input-list" :options="['Finance', 'Sales', 'Support','Admin' ]"></b-form-datalist>
 
      <label><small>Agent</small></label>
     <b-form-input ></b-form-input>
    </b-form>
 
 
-</div>
+
 
     </b-col>
-    <b-col cols="3">
+    </div>
+    <b-col cols="">
 
-<div>
+<div id="cad">
   
     <b-card
       header="Contact Details"
       header-tag="header"
+       
       
-      title="Title"
     >
       <b-card-text>
       <b-row>
           <b-col cols="3">
-              <span class="badge badge-secondary">C</span>
+              <span class="badge badge-success" >C</span>
           </b-col>
          
-          <b-col>
-               Chris
+          <b-col id="na">
+              <p> Chris</p>
+
+              <i><small>Email</small></i>
+               Kikie@itrack.com
               </b-col>
       </b-row>
-     <p><i><small>Email</small></i></p>
-     Kikie@itrack.com
+    
       </b-card-text>
-      <b-button href="#" variant="primary">Go somewhere</b-button>
+      <b-button href="#" variant="success">view contact</b-button>
     </b-card>
 </div>
 
@@ -161,3 +150,35 @@ export default {
   }
 }
 </script>
+<style>
+#ticket{
+  background: white;
+  margin-right: 2px;
+  text-align: left;
+}
+
+#res{
+  background: white;
+  text-align: left;
+  height: 70vh;
+  overflow-y: scroll;
+
+}
+
+#ticket:hover{
+  transform: scale(0.9);
+  
+}
+#ket{
+  margin-top: 10px;
+}
+#navi{
+
+  background-color: white;
+}#na{
+  text-align: left;
+}
+#na:hover{
+   transform: scale(1.1);
+}
+</style>
